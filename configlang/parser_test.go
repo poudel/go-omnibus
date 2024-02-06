89d4a33d476ee38d37904a44a28a0f0ee30f59c1
Keshab Paudel <self@keshab.net>
2024-02-06 02:06:32 +0100

    remove str

--- configlang/parser_test.go
+++ configlang/parser_test.go
@@ -7,23 +7,7 @@ import (
 
 func TestParseConfig(t *testing.T) {
 	c := `
-name : str = "K Paudel"				// explicit type
-website := "https://keshab.net"		// type inferred
-
-// This is a dictionary
-address = {
-  	timezone = "Asia/Kathmandu",
-
-	// this is a comment
-	// or, is it?
-	city = "Kathmandu",
-}
-
-// This is a list
-phones = {
-	9842002937,
-	9483427344
-}`
+`
 	fmt.Println(c)
 
 }
